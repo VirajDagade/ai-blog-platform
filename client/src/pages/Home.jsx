3ce44eb7639de8893642bf5d52492d275d2a0cff
Viraj Dagade <virajdagade25@gmail.com>
2025-07-23 18:52:55 +0530

    Fix: Corrected Footer import case issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import BlogList from "../components/BlogList";
 import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer.jsx";
 
 function Home() {
   return (
